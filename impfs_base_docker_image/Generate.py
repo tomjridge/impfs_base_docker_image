c5975f76a9a8acebd62bebf75f6e0617a69aebbf
Tom Ridge <tom.j.ridge@googlemail.com>
2018-10-24 17:22:55 +0100

    update to opam2 ub18.04

--- impfs_base_docker_image/Generate.py
+++ impfs_base_docker_image/Generate.py
@@ -12,15 +12,14 @@ opam_deps()
 
 # rest -----------------------------------------------------------------
 
-out="""FROM ubuntu_16.04_with_ocaml_4.06
+out="""FROM ubuntu_18.04_with_opam2_ocaml_4.06
 
 RUN opam list
-RUN ocamlfind list
 RUN sudo apt-get -y install libfuse-dev
 
 """
 
-out+="RUN opam install "+' '.join(sorted(list(opam_deps())))+"\n"
+out+="RUN opam install -y "+' '.join(sorted(list(opam_deps())))+"\n"
 
 # print(out)
 
